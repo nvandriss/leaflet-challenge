5f1e9f3e2b57e67888a21f3563c92d9a6e7b77e9
Nicole Van Driss <nvandriss@gmail.com>
2022-01-12 16:58:18 -0500

    Update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,83 +1,98 @@
-    // Create initial map
-    const myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 4,
-    });
-    //Adding a tile layer/background map image
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-    //Store API endpoint
-    var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-    d3.json(queryUrl, function(data) {
-function styleInfo(feature) {
-    return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
-        color: "#000000",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-    };
-    }
-    // Set color
-    function getColor(magnitude) {
-    switch (true) {
-    case magnitude > 5:
-        return "#ea2c2c";
-    case magnitude > 4:
-        return "#ea822c";
-    case magnitude > 3:
-        return "#ee9c00";
-    case magnitude > 2:
-        return "#eecc00";
-    case magnitude > 1:
-        return "#d4ee00";
-    default:
-        return "#98ee00";
-    }
-    }
-    // Set radius
-    function getRadius(magnitude) {
-    if (magnitude === 0) {
-        return 1;
-    }
-    return magnitude * 4;
-    }
-    //GeoJSON layer
-    L.geoJson(data, {
-    pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-        },
-        style: styleInfo,
-        onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-        }
-    }).addTo(myMap);
-    //Legend
-    var legend = L.control({
-        position: "bottomright"
+async function main () {
+    const bgmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         });
-        legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend");
-        var grades = [0, 1, 2, 3, 4, 5];
-        var colors = [
-            "#98ee00",
-            "#d4ee00",
-            "#eecc00",
-            "#ee9c00",
-            "#ea822c",
-            "#ea2c2c"
-        ];
-        //Loop
-        for (var i = 0; i < grades.length; i++) {
-            div.innerHTML +=
-            "<i style='background: " + colors[i] + "'></i> " +
-            grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      
+      const map = L.map("map", {
+        center: [
+          40.7, -94.5
+        ],
+        zoom: 3
+      });
+      
+      bgmap.addTo(map);
+            d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson").then(function(data) {
+      
+        function styleInfo(feature) {
+          return {
+            opacity: 1,
+            fillOpacity: 0.75,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 1
+          };
         }
-        return div;
-    };
-    //Add Legend
-    legend.addTo(myMap);
-})
+      
+        function getColor(depth) {
+          switch (true) {
+          case depth > 90:
+            return "#e92684";
+          case depth > 70: 
+            return "#e95d26"; 
+          case depth > 50:
+            return "#ee9c00";
+          case depth > 30:
+            return "#eecc00";
+          case depth > 10:
+            return "#d4ee00";
+          default:
+            return "#98ee00";
+          }
+        }
+      
+        function getRadius(magnitude) {
+          if (magnitude === 0) {
+            return 1;
+          }
+          return magnitude * 4;
+        }
+      
+      
+        L.geoJson(data, {
+          pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+          },
+          style: styleInfo,
+          onEachFeature: function(feature, layer) {
+            layer.bindPopup(
+              "Magnitude: "
+                + feature.properties.mag
+                + "<br>Depth: "
+                + feature.geometry.coordinates[2]
+                + "<br>Location: "
+                + feature.properties.place
+            );
+          }
+        }).addTo(map);
+      
+        const legend = L.control({
+          position: "bottomright"
+        });
+      
+      legend.onAdd = function() {
+          const div = L.DomUtil.create("div", "info legend");
+      
+          const grades = [-10, 10, 30, 50, 70, 90];
+          const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#e95d26",
+        "#e92684"
+          ];
+      
+          for (let i = 0; i < grades.length; i++) {
+            div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+            + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+          }
+          return div;
+        };
+      
+        legend.addTo(map);
+      });
+}
+
+    main();
